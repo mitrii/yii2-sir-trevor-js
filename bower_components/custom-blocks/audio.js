e61175dc2792be479095a12f9ea15e66dcbf2226
mitrii <p.mitrii@gmail.com>
2016-03-16 18:35:11 +0600

    add icons

--- bower_components/custom-blocks/audio.js
+++ bower_components/custom-blocks/audio.js
@@ -1,48 +1,48 @@
 SirTrevor.Blocks.Audio = SirTrevor.Block.extend({
 
-  type: "audio",
-  title: function() { return 'Аудиофайл'; },
+    type: "audio",
+    title: function() { return 'Аудиофайл'; },
 
-  droppable: true,
-  uploadable: true,
+    droppable: true,
+    uploadable: true,
 
-  icon_name: 'image',
+    icon_name: 'file-music',
 
-  loadData: function(data){
-    // Create our image tag
-    this.$editor.html($('<audio>', { src: data.file.url }));
-  },
+    loadData: function(data){
+        // Create our image tag
+        this.$editor.html($('<audio>', { src: data.file.url }));
+    },
 
-  onBlockRender: function(){
-    /* Setup the upload button */
-    this.$inputs.find('button').bind('click', function(ev){ ev.preventDefault(); });
-    this.$inputs.find('input').on('change', (function(ev) {
-      this.onDrop(ev.currentTarget);
-    }).bind(this));
-  },
+    onBlockRender: function(){
+        /* Setup the upload button */
+        this.$inputs.find('button').bind('click', function(ev){ ev.preventDefault(); });
+        this.$inputs.find('input').on('change', (function(ev) {
+            this.onDrop(ev.currentTarget);
+        }).bind(this));
+    },
 
-  onDrop: function(transferData){
-    var file = transferData.files[0],
-        urlAPI = (typeof URL !== "undefined") ? URL : (typeof webkitURL !== "undefined") ? webkitURL : null;
+    onDrop: function(transferData){
+        var file = transferData.files[0],
+            urlAPI = (typeof URL !== "undefined") ? URL : (typeof webkitURL !== "undefined") ? webkitURL : null;
 
-    // Handle one upload at a time
-    if (/image/.test(file.type)) {
-      this.loading();
-      // Show this image on here
-      this.$inputs.hide();
-      this.$editor.html($('<audio>', { src: urlAPI.createObjectURL(file) })).show();
+        // Handle one upload at a time
+        if (/image/.test(file.type)) {
+            this.loading();
+            // Show this image on here
+            this.$inputs.hide();
+            this.$editor.html($('<audio>', { src: urlAPI.createObjectURL(file) })).show();
 
-      this.uploader(
-        file,
-        function(data) {
-          this.setData(data);
-          this.ready();
-        },
-        function(error) {
-          this.addMessage(i18n.t('blocks:image:upload_error'));
-          this.ready();
+            this.uploader(
+                file,
+                function(data) {
+                    this.setData(data);
+                    this.ready();
+                },
+                function(error) {
+                    this.addMessage(i18n.t('blocks:image:upload_error'));
+                    this.ready();
+                }
+            );
         }
-      );
     }
-  }
 });
